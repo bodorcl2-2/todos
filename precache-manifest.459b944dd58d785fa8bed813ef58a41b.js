5c1acef71f657039aa1232fd2bc6b203c23a3c13
darekrcl <darek.rcl@gmail.com>
2020-05-12 18:32:20 +0200

    Updates

---
+++ precache-manifest.459b944dd58d785fa8bed813ef58a41b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02da0b7b447edb1daefd68caf6f546b8",
+    "url": "/todos/index.html"
+  },
+  {
+    "revision": "6317075e978aff2fb9ae",
+    "url": "/todos/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "bc9e179cef1b0f27010e",
+    "url": "/todos/static/js/2.a1841def.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/todos/static/js/2.a1841def.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6317075e978aff2fb9ae",
+    "url": "/todos/static/js/main.454d450a.chunk.js"
+  },
+  {
+    "revision": "3897dab5e55f80542c21",
+    "url": "/todos/static/js/runtime-main.b6c34825.js"
+  }
+]);
